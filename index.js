a6d2d4485ef524b682d57d006d528e33a7de39da
Abul Kalam Azad <azad@xpeedstudio.com>
2022-12-17 16:19:26 +0600

    Add frontend part main js and style an index.html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+const path = require('path');
 // app initializations
 const app = express();
 // Middleware use (enable body parser)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //app listen port 5000
 app.use('/openai', require('./routes/openaiRoutes'));
